be983c171b7022384556d3ddfb0a95bc72a27029
James King <james.kibg020773@gmail.com>
2024-03-14 11:19:41 +1100

    User Authentication Added

--- eduprod/models.py
+++ eduprod/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    answer_text = models.TextField()
+
+    def __str__(self):
+        return self.question_text
--- eduprod/views.py
+++ eduprod/views.py
@@ -1,7 +1,9 @@
 from django.core import serializers
 from django.shortcuts import render
 from .models import Question
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
     questions = Question.objects.all()
     questions_json = serializers.serialize('json', questions)
